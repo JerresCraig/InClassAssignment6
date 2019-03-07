66113a6b4bb5f82d5eb4e5f4ebe65e0d78f74e21
Jerres <Jerres@JERRESLAPTOP>
2019-03-07 13:35:02 -0700

    Jerres Craig

--- InClassAssignment6/Form1.Designer.cs
+++ InClassAssignment6/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRollDice = new System.Windows.Forms.Button();
+            this.lblD1 = new System.Windows.Forms.Label();
+            this.lblD2 = new System.Windows.Forms.Label();
+            this.lblNumRolls = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRollDice
+            // 
+            this.btnRollDice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRollDice.Location = new System.Drawing.Point(112, 291);
+            this.btnRollDice.Name = "btnRollDice";
+            this.btnRollDice.Size = new System.Drawing.Size(262, 61);
+            this.btnRollDice.TabIndex = 0;
+            this.btnRollDice.Text = "Roll Dice";
+            this.btnRollDice.UseVisualStyleBackColor = true;
+            this.btnRollDice.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblD1
+            // 
+            this.lblD1.AutoSize = true;
+            this.lblD1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblD1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblD1.Location = new System.Drawing.Point(112, 97);
+            this.lblD1.Name = "lblD1";
+            this.lblD1.Size = new System.Drawing.Size(125, 135);
+            this.lblD1.TabIndex = 1;
+            this.lblD1.Text = "#";
+            this.lblD1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblD2
+            // 
+            this.lblD2.AutoSize = true;
+            this.lblD2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblD2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblD2.Location = new System.Drawing.Point(249, 97);
+            this.lblD2.Name = "lblD2";
+            this.lblD2.Size = new System.Drawing.Size(125, 135);
+            this.lblD2.TabIndex = 2;
+            this.lblD2.Text = "#";
+            this.lblD2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNumRolls
+            // 
+            this.lblNumRolls.AutoSize = true;
+            this.lblNumRolls.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumRolls.Location = new System.Drawing.Point(222, 355);
+            this.lblNumRolls.Name = "lblNumRolls";
+            this.lblNumRolls.Size = new System.Drawing.Size(51, 55);
+            this.lblNumRolls.TabIndex = 3;
+            this.lblNumRolls.Text = "#";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(499, 450);
+            this.Controls.Add(this.lblNumRolls);
+            this.Controls.Add(this.lblD2);
+            this.Controls.Add(this.lblD1);
+            this.Controls.Add(this.btnRollDice);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.Label lblD1;
+        private System.Windows.Forms.Label lblD2;
+        private System.Windows.Forms.Label lblNumRolls;
     }
 }
 
--- InClassAssignment6/Form1.cs
+++ InClassAssignment6/Form1.cs
@@ -10,11 +10,56 @@ using System.Windows.Forms;
 
 namespace InClassAssignment6
 {
+
+
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+        //Jerres Craig
+        //This is my own work
+
+        //send link to github and submit link
+        //page 563 in textbook for UML
+        //dice class
+        class Dice
+        {
+            private int numSides;
+            static Random rnd = new Random();
+            public Dice(int n)
+            {
+                numSides = n;
+            }
+            public int rollDie()
+            {
+                return rnd.Next() % numSides + 1;
+            }
+        }
+        int numRolls = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dice d1 = new Dice(6);
+            Dice d2 = new Dice(6);
+
+            lblD1.Text = d1.rollDie().ToString();
+            lblD2.Text = d2.rollDie().ToString();
+
+            numRolls++;
+            lblNumRolls.Text = numRolls.ToString();
+
+            //snake eyes
+            if (lblD1.Text == "1" && lblD2.Text == "1")
+            {
+                MessageBox.Show("It took " + numRolls + " rolls to get snake eyes!");
+                btnRollDice.Enabled = false;
+            }
+
+
+        }
     }
+
 }
